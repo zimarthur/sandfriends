dfbd992c26575f00752ace12f02ace534bb18595
zimarthur <zim.arthur97@gmail.com>
2022-08-12 16:43:56 -0300

    Adição de função userFromJson

--- FrontEnd/sandfriends/lib/models/user.dart
+++ FrontEnd/sandfriends/lib/models/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class User with ChangeNotifier {
   String? FirstName;
@@ -10,4 +11,17 @@ class User with ChangeNotifier {
   double? Height;
   String? HandPreference;
   String? Photo;
+}
+
+void userFromJson(BuildContext context, Map<String, dynamic> json) {
+  Provider.of<User>(context, listen: false).FirstName = json['FirstName'];
+  Provider.of<User>(context, listen: false).LastName = json['LastName'];
+  Provider.of<User>(context, listen: false).Gender = json['Gender'];
+  Provider.of<User>(context, listen: false).PhoneNumber = json['PhoneNumber'];
+  Provider.of<User>(context, listen: false).Birthday = json['Birthday'];
+  Provider.of<User>(context, listen: false).Rank = json['Rank'];
+  Provider.of<User>(context, listen: false).Height = json['Height'];
+  Provider.of<User>(context, listen: false).HandPreference =
+      json['HandPreference'];
+  Provider.of<User>(context, listen: false).Photo = json['Photo'];
 }
