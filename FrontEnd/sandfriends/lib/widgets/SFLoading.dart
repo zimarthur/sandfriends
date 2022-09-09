595ad51852f2fd5140d684539818dc6e08d8d6c7
zimarthur <zim.arthur97@gmail.com>
2022-09-09 20:13:15 -0300

    widget loading

---
+++ FrontEnd/sandfriends/lib/widgets/SFLoading.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+import '../theme/app_theme.dart';
+
+class SFLoading extends StatelessWidget {
+  const SFLoading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return LoadingAnimationWidget.flickr(
+      leftDotColor: AppTheme.colors.primaryBlue,
+      rightDotColor: AppTheme.colors.secondaryYellow,
+      size: width * 0.1,
+    );
+  }
+}
