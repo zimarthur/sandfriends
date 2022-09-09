db2e03da29ebc41568beea8953ff83885e108df3
zimarthur <zim.arthur97@gmail.com>
2022-09-09 20:15:43 -0300

    adição photo para esporte

--- FrontEnd/sandfriends/lib/models/sport.dart
+++ FrontEnd/sandfriends/lib/models/sport.dart
@@ -1,6 +1,10 @@
 class Sport {
   final int idSport;
   final String description;
+  final String photoUrl;
 
-  Sport({required this.idSport, required this.description});
+  Sport(
+      {required this.idSport,
+      required this.description,
+      required this.photoUrl});
 }
