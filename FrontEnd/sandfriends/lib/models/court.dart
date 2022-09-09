b5033bf459d048c1252b09a2e48daa144e7f044c
zimarthur <zim.arthur97@gmail.com>
2022-09-09 20:13:53 -0300

    adição nome da quadra

--- FrontEnd/sandfriends/lib/models/court.dart
+++ FrontEnd/sandfriends/lib/models/court.dart
@@ -1,25 +1,9 @@
 import 'package:sandfriends/models/court_available_hours.dart';
 
-import 'store.dart';
-
 class Court {
-  int _index = 0;
-  int get index => _index;
-  set index(int value) {
-    _index = value;
-  }
-
-  Store _store = Store();
-  Store get store => _store;
-  set store(Store value) {
-    _store = value;
-  }
-
-  String _day = "sf";
-  String get day => _day;
-  set day(String value) {
-    _day = value;
-  }
-
+  final int idStoreCourt;
+  final String storeCourtName;
   List<CourtAvailableHours> availableHours = [];
+
+  Court(this.idStoreCourt, this.storeCourtName);
 }
