5adaa78f4d2b088c20fa761bbeeaa60b5b08c54a
zimarthur <zim.arthur97@gmail.com>
2022-09-22 21:57:52 -0300

    Criado Court Provider

---
+++ FrontEnd/sandfriends/lib/providers/court_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../models/court.dart';
+
+class CourtProvider with ChangeNotifier {
+  List<Court> _courts = [];
+  List<Court> get courts => _courts;
+  void addCourt(Court court) {
+    bool isNewCourt = true;
+    for (int i = 0; i < _courts.length; i++) {
+      if (_courts[i].idStoreCourt == court.idStoreCourt) {
+        isNewCourt = false;
+        break;
+      }
+    }
+    if (isNewCourt) {
+      _courts.add(court);
+      notifyListeners();
+    }
+  }
+
+  void clearCourts() {
+    _courts.clear();
+    notifyListeners();
+  }
+}
