e693e944c9c78a7e1b369ebbd48a08b847574b12
zimarthur <zim.arthur97@gmail.com>
2022-09-22 21:55:11 -0300

    Adicionado parametro IsIndoor

--- FrontEnd/sandfriends/lib/models/court.dart
+++ FrontEnd/sandfriends/lib/models/court.dart
@@ -3,7 +3,8 @@ import 'package:sandfriends/models/court_available_hours.dart';
 class Court {
   final int idStoreCourt;
   final String storeCourtName;
+  final bool isIndoor;
   List<CourtAvailableHours> availableHours = [];
 
-  Court(this.idStoreCourt, this.storeCourtName);
+  Court(this.idStoreCourt, this.storeCourtName, this.isIndoor);
 }
